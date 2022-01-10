b9517fae1085055dca72ae3d8b003597f1ee3288
chrisleitzinger <christelleleitzinger@gmail.com>
2022-01-10 12:53:50 -0500

    Update data

--- R/01.Load data.R
+++ R/01.Load data.R
@@ -10,41 +10,41 @@ library(gtsummary)
 path <- fs::path("", "Volumes", "Gillis_Research","Christelle Colin-Leitzinger", "Breast CH")
 
 Demographic <- 
-  readxl::read_xlsx(paste0(path, "/raw data/Breast Cancer with Blood samples .xlsx"),
+  readxl::read_xlsx(paste0(path, "/raw data/Breast+Sarcoma pts with DNA samples_PHI_01072020.xlsx"),
                     sheet = "PTE Demographics") %>% 
   janitor::clean_names()
 
 breast_DNA <- # Updated_10R21000238_De-identified_for_Breast_pats_wDNA.xlsx
-  readxl::read_xlsx(paste0(path, "/raw data/Breast Cancer with Blood samples .xlsx"),
-                    sheet = "Biobanking") %>% 
+  readxl::read_xlsx(paste0(path, "/raw data/Breast+Sarcoma pts with DNA samples_PHI_01072020.xlsx"),
+                    sheet = "BB_Breast pts samples") %>% 
   janitor::clean_names()
 
 breast_info <- 
-  readxl::read_xlsx(paste0(path, "/raw data/Breast Cancer with Blood samples .xlsx"),
-                    sheet = "PTE CR") %>% 
+  readxl::read_xlsx(paste0(path, "/raw data/Breast+Sarcoma pts with DNA samples_PHI_01072020.xlsx"),
+                    sheet = "PTE_Cancer Characteristics") %>% 
   janitor::clean_names()
 
 Chemot <- 
-  readxl::read_xlsx(paste0(path, "/raw data/Breast Cancer with Blood samples .xlsx"),
-                    sheet = "Treatment Chemotherapy") %>% 
+  readxl::read_xlsx(paste0(path, "/raw data/Breast+Sarcoma pts with DNA samples_PHI_01072020.xlsx"),
+                    sheet = "Treatment_Chemotherapy") %>% 
   janitor::clean_names()
 
 Hormonet <- 
-  readxl::read_xlsx(paste0(path, "/raw data/Breast Cancer with Blood samples .xlsx"),
+  readxl::read_xlsx(paste0(path, "/raw data/Breast+Sarcoma pts with DNA samples_PHI_01072020.xlsx"),
                     sheet = "Treatment Hormone") %>% 
   janitor::clean_names()
 
 Immnunot <- 
-  readxl::read_xlsx(paste0(path, "/raw data/Breast Cancer with Blood samples .xlsx"),
-                    sheet = "Treatment Immunotherapy") %>% 
+  readxl::read_xlsx(paste0(path, "/raw data/Breast+Sarcoma pts with DNA samples_PHI_01072020.xlsx"),
+                    sheet = "Treatment_Immuno") %>% 
   janitor::clean_names()
 
 Radiot <- 
-  readxl::read_xlsx(paste0(path, "/raw data/Breast Cancer with Blood samples .xlsx"),
-                    sheet = "Treatment Radiation") %>% 
+  readxl::read_xlsx(paste0(path, "/raw data/Breast+Sarcoma pts with DNA samples_PHI_01072020.xlsx"),
+                    sheet = "Treatment_Radiation") %>% 
   janitor::clean_names()
 
-Second_dx <- read_csv(paste0(path, "/raw data/Data_export_for_De-identified_PTE_CR_14-Sep-2021.csv")) %>% 
-  janitor::clean_names() %>% 
-  select(-group_name)
+# Second_dx <- read_csv(paste0(path, "/raw data/Data_export_for_De-identified_PTE_CR_14-Sep-2021.csv")) %>% 
+#   janitor::clean_names() %>% 
+#   select(-group_name)
 
