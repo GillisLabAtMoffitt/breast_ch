903312c58839e26d867b8406708b53966ca7a14f
chrisleitzinger <christelleleitzinger@gmail.com>
2022-06-06 18:55:35 -0400

    Add script to select samples from what is available in the tissue bank.

---
+++ R/05.select 20 samples.R
@@ -0,0 +1,81 @@
+# Import Library
+library(tidyverse)
+library(here)
+library(lubridate)
+
+
+################################################################################# I ### Load data
+blood_patients <- 
+  read_rds(paste0(here(), "/blood_patients.rds"))
+
+available <- 
+  readxl::read_xlsx(paste0(here(), "/EXP1581DNA - Dr. Gillis 21545 Datasheet_w locations.xlsx"),
+                    skip = 9, n_max = 74
+                    ) %>% 
+  janitor::clean_names()
+
+
+################################################################################# II ### Merge
+
+blood_patients <- blood_patients %>% 
+  # group_by(mrn, party_id, sample_family_id, specimen_collection_date) %>% 
+  separate(col = sample_id, into = paste("sample_id", 1:30, sep="_"), sep = "; ") %>% 
+  purrr::keep(~!all(is.na(.))) %>%
+# separate(col = drug_name_, paste("drug_name_", 1:7, sep=""), sep = ";", extra = "warn", # Just for 1 row
+#          fill = "right")
+  arrange(mrn, specimen_collection_date) %>% 
+  pivot_longer(cols = starts_with("sample_id"),
+               names_to = "sample_sequence", 
+               values_to = "sample_id", 
+               values_drop_na = TRUE) %>% 
+  select("mrn", "deidentified_patient_id", "party_id",
+         "sample_family_id", "sample_sequence", "sample_id",
+         "specimen_collection_date", everything())
+
+available <- available %>% 
+  select(-c("sample_run_number", "lv_alias_dna_id", "surgical_no",
+            "study")) %>% 
+  mutate(sample_lv = str_to_lower(sample_lv))
+
+breast_data <- left_join(available #%>% 
+                           # select(-c("sample_run_number", "lv_alias_dna_id", "surgical_no",
+                           #           "study")) %>% 
+                           # mutate(sample_lv = str_to_lower(sample_lv))
+                         , 
+                         blood_patients%>% 
+                           mutate(sample_family_id = str_to_upper(sample_family_id)),
+                         by = c("mrn", "sample_family_id", "sample_lv" = "sample_id"))
+
+
+breast_data <- breast_data %>% 
+  arrange(mrn, specimen_collection_date) %>% 
+  distinct(mrn, clinical_event, .keep_all = TRUE) %>% 
+  mutate(specimen_sequence = row_number()) %>%
+  group_by(mrn) %>% 
+  mutate(n = n()) %>% 
+  filter(n > 1) %>% 
+  group_by(mrn, clinical_event) %>% 
+  mutate(specimen_sequence = row_number()) %>%
+  ungroup() %>%
+  mutate(age_at_sample = round(interval(start = date_of_birth, end = specimen_collection_date)/
+                                 duration(n = 1, units = "years"), 1)
+  ) %>% 
+  select("mrn", "deidentified_patient_id", "party_id",
+         age_at_diagnosis, age_at_sample,
+         clinical_event, chemotherapy_drug_1,
+         specimen_sequence,
+         sample_id = "sample_lv", "sample_family_id",
+         "specimen_collection_date",
+         "immediate_parent_sample_id",
+         "dna_master_lv_id",
+         "dna_aliquot_lv_id",
+         "original_parent_sample_id", everything(),
+         -c("sample_sequence", n))
+
+write_csv(breast_data, paste0(here(), "/sample lists/list of samples available in the bank.csv"))
+
+
+
+
+
+
